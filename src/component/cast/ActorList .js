1b526e33d1d7a5c8d96f1322c5b5d011658caaa8
DIILEESHA <dileeeshanawarathna18@gmail.com>
2023-12-11 02:57:33 +0530

    added defalt image to serch result && contentdetail component. some images are not store in api and not fetch properly

--- src/component/cast/ActorList .js
+++ src/component/cast/ActorList .js
@@ -1,18 +1,26 @@
 // ActorList.js
 import React from "react";
-import './actor.css'
+import "./actor.css";
 
-const ActorList = ({ cast,name }) => {
+const ActorList = ({ cast, name }) => {
   return (
     <div className="actor-list">
       <h3 className="actor ">Top Cast</h3>
       <ul>
         {cast.map((actor) => (
           <li key={actor.id}>
-            <img
-              src={`https://image.tmdb.org/t/p/w200/${actor.profile_path}`}
-              alt={actor.name}
-            />
+            {actor.profile_path ? (
+              <img
+                src={`https://image.tmdb.org/t/p/w200/${actor.profile_path}`}
+                alt={actor.name}
+              />
+            ) : (
+              <img
+                src="https://ehelperteam.com/wp-content/uploads/2019/09/Broken-images.png"
+                alt={actor.name}
+                style={{ opacity: "0.6" }}
+              />
+            )}
             <p>{actor.name}</p>
           </li>
         ))}
